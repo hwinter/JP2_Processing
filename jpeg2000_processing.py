31121f33d5f3ee4e60f7028d6e1ecf0049935faf
MilesGordon <miles@miredmusic.com>
2018-04-05 13:53:25 -0400

    First Commit!
    This is a separate repo for playing with realtime processing of a
    stream of jp2 images from SDO. This code will filter back in to the AIA
    daily movie generation codebase.

---
+++ jpeg2000_processing.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+
+import matplotlib
+matplotlib.use('agg')
+
+from moviepy.editor import *
+from moviepy.video.tools.segmenting import findObjects
+from PIL import ImageFont, ImageDraw, Image
+from astropy.io import fits
+from sys import stdout as stdout
+from numba import jit
+from multiprocessing import Pool
+
+import numpy as np
+import sunpy.instr.aia as aia
+import matplotlib.pyplot as plt
+
+import cv2
+import subprocess
+import glob
+import os
+import datetime
+import sys
+
+def Fits_Index(DIR):
+	fits_list = []
+	count = 0
+	for fits_file in sorted(glob.glob(DIR + "/*.jp2")):
+		print("\r Adding file: " + str(fits_file) + " Entries: " + str(count), end = " ")
+		fits_list.append(str(fits_file))
+		count = count + 1
+	print(fits_list) 
+	return(fits_list)
+
+def AIA_DecimateIndex(LIST, SKIP):
+	list_in = LIST
+	print("DECIMATING")
+	list_out = [list_in[i] for i in xrange(0, len(list_in), SKIP)]
+
+	return(list_out)
+
+def Colorize(FILE):
+	sorted_number = sorted_list.index(FILE)
+	print("CONVERTING: " + str(FILE))
+	subprocess.call("convert " + str(FILE) + " gradient_ice-sea.png -clut numbered/" + str(sorted_number) + ".png", shell = True)
+	
+
+
+sorted_list = Fits_Index("working")
+sorted_list = AIA_DecimateIndex(sorted_list, 4)
+
+if os.path.isdir("numbered") == False:
+	subprocess.call("mkdir numbered" , shell = True)
+
+# sorted_number = 0
+
+# for f in sorted_list:
+# 	print("CONVERTING: " + str(f))
+# 	subprocess.call("convert " + str(f) + " gradient_ice-sea.png -clut numbered/" + str(sorted_number) + ".png", shell = True)
+# 	sorted_number += 1
+
+		# Using multiprocess.pool() to parallelize our frame rendering
+
+pool = Pool()
+pool.map(Colorize, sorted_list)
+pool.close()
+pool.join()
+
