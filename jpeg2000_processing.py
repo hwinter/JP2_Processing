a4ea563deeafa885c4d734dca6c6f43793872a3a
MilesGordon <miles@miredmusic.com>
2018-04-30 21:52:19 -0400

    Minor Value Adjustments

--- jpeg2000_processing.py
+++ jpeg2000_processing.py
@@ -94,7 +94,7 @@ def Annotate(FILE):
 
 for wlen in target_wavelengths:
 	sorted_list = Fits_Index(str(wlen))
-	sorted_list = AIA_DecimateIndex(sorted_list, 16)
+	sorted_list = AIA_DecimateIndex(sorted_list, 2)
 
 	current_wavelength = wlen
 	
