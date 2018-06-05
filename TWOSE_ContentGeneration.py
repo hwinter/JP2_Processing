f78587a986c6367e145e8cb488f37e495433f3b5
MilesGordon <miles@miredmusic.com>
2018-06-04 20:59:38 -0400

    bugs

--- TWOSE_ContentGeneration.py
+++ TWOSE_ContentGeneration.py
@@ -128,8 +128,8 @@ def Add_Earth(FILE):
 	main_video.extend( [earth_g] )
 	out_video = CompositeVideoClip(main_video)
 
-	out_video.set_duration(mlength).write_videofile(str(FILE), fps = 24, threads = 2, audio = False, progress_bar = True)
-	# os.rename("o_" + str(FILE),FILE)
+	out_video.set_duration(mlength).write_videofile(str(FILE).split(".")[0] + "_.mp4", fps = 24, threads = 2, audio = False, progress_bar = True)
+	os.rename(str(FILE).split(".") + "_.mp4", FILE)
 
 ##FROM AIA
 def Video_List():
