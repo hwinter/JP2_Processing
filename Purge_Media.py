5fddea1409292c526a0b6f77d696896cbfcb5831
MilesGordon <miles@miredmusic.com>
2018-06-07 20:04:51 -0400

    Textiness

--- Purge_Media.py
+++ Purge_Media.py
@@ -2,15 +2,17 @@ import subprocess
 import glob
 import os
 
-target_wavelengths = ["94", "171", "193", "211", "304", "335"]
+def Purge_Media():
+	target_wavelengths = ["94", "171", "193", "211", "304", "335"]
 
-for wlen in target_wavelengths:
-	
-	for f in glob.glob(str(wlen) + "/*.png"):
-		    os.remove(f)
-	for f in glob.glob(str(wlen) + "/*.jp2"):
-		    os.remove(f)
-	for f in glob.glob(str(wlen) + "/*.1"):
-		    os.remove(f)	
+	for wlen in target_wavelengths:
 
+		print("PURGING: " + str(wlen))
+		
+		for f in glob.glob(str(wlen) + "/*.png"):
+			    os.remove(f)
+		for f in glob.glob(str(wlen) + "/*.jp2*"):
+			    os.remove(f)
 
+if __name__ == "__main__":
+	Purge_Media()
